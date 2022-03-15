790f976613fe785e7370400619bdc9d5f5d59a54
jonasdarruda <jonasdarruda@gmail.com>
2022-03-15 15:38:52 -0300

    Exercicio2

---
+++ aula01/Exercicio02.java
@@ -0,0 +1,36 @@
+import java.nio.file.ClosedWatchServiceException;
+import java.util.Scanner;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Exercicio02 {
+
+    public static void main(String[] args){
+        Scanner teclado new Scanner(System.in);
+
+        double salario, prestacao, emprestimo
+
+        System.out.println("Digite o valor do salario ");
+        salario = teclado.nextDouble();
+
+        System.out.println("Digite o valor da prestação ");
+        prestacao = teclado.nextDouble();
+
+        emprestimo = salario * 0.3;
+
+        if(prestacao <= emprestimo){
+            System.out.println("Você pode receber o emprestimo ");
+        } else {
+            System.out.println( "Você não pode receber o emprestimo");
+        }
+
+
+
+
+
+
+        teclado.close();
+
+    }
+    
+}
